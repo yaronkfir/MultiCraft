18b7bd4bb1134d0fcbf919be1091f54aaabed810
Shai Oren <shai@shaioren.net>
2014-05-06 23:43:36 -0700

    Change file encoding to UTF-8

--- MultiCraft/localization/DE.lua
+++ MultiCraft/localization/DE.lua
@@ -3,10 +3,10 @@ local SI = MultiCraftAddon.SI
 
 mkstr(SI.USAGE_1,			"MultiCraft Benutzung:")
 mkstr(SI.USAGE_2,			"/toggle: Schaltet den Standard des Reglers zwischen 1 und Max um. Standard ist 1.")
-mkstr(SI.USAGE_3,			"/trait: Schaltet um, ob die mehrfache Herstellung von Gegenst‰nden mit Eigenschaften erlaubt ist. Standard ist aktiviert.")
-mkstr(SI.USAGE_4,			"/delay N: Dies verzˆgert alle Handwerksaktivit‰ten um den angegebenen Wert in Millisekunden. Standard ist 500.")
+mkstr(SI.USAGE_3,			"/trait: Schaltet um, ob die mehrfache Herstellung von Gegenst√§nden mit Eigenschaften erlaubt ist. Standard ist aktiviert.")
+mkstr(SI.USAGE_4,			"/delay N: Dies verz√∂gert alle Handwerksaktivit√§ten um den angegebenen Wert in Millisekunden. Standard ist 500.")
 mkstr(SI.DEFAULT_MAX,		"MultiCraft: Reglerstandard ist jetzt Max.")
 mkstr(SI.DEFAULT_MIN,		"MultiCraft: Reglerstandard ist jetzt 1.")
 mkstr(SI.TRAITS_ON,			"MultiCraft: Eigenschaften aktiviert.")
 mkstr(SI.TRAITS_OFF,		"MultiCraft: Eigenschaften deaktiviert.")
-mkstr(SI.CALL_DELAY,		"MultiCraft: Handwerksaktivit‰ten limitiert auf einen Aufruf alle %d Millisekunden.")
+mkstr(SI.CALL_DELAY,		"MultiCraft: Handwerksaktivit√§ten limitiert auf einen Aufruf alle %d Millisekunden.")
