c954e025d7d993e7517f28cf6379d705216f2902
Shai Oren <shai@shaioren.net>
2014-05-11 22:52:07 -0700

    French localization

--- MultiCraft/localization/FR.lua
+++ MultiCraft/localization/FR.lua
@@ -1,12 +1,12 @@
 local mkstr = ZO_CreateStringId
 local SI = MultiCraftAddon.SI
 
-mkstr(SI.USAGE_1,			"MultiCraft Usage:")
-mkstr(SI.USAGE_2,			"/toggle: Toggle slider defaults between 1 and max. Default is 1.")
-mkstr(SI.USAGE_3,			"/trait: Toggle whether using traits allows crafting multiple items. Default is enabled.")
-mkstr(SI.USAGE_4,			"/delay N: This delays all crafting calls by the specified value in milliseconds. Default is 500.")
-mkstr(SI.DEFAULT_MAX,		"MultiCraft: Slider defaults to max.")
-mkstr(SI.DEFAULT_MIN,		"MultiCraft: Slider defaults to 1.")
-mkstr(SI.TRAITS_ON,			"MultiCraft: Traits enabled.")
-mkstr(SI.TRAITS_OFF,		"MultiCraft: Traits disabled.")
-mkstr(SI.CALL_DELAY,		"MultiCraft: Crafting rate limited to one call every %d milliseconds.")
+mkstr(SI.USAGE_1,			"Utilisation de MultiCraft:")
+mkstr(SI.USAGE_2,			"/toggle: Alterne les valeurs par défaut du slider entre 1 et max. La valeur par défaut est 1.")
+mkstr(SI.USAGE_3,			"/trait: Active/désactive l'utilisation des caractéristiques d'objets lors de la fabrication d'objets multiples. Activé par défaut.")
+mkstr(SI.USAGE_4,			"/delay N: Applique un délai de N millisecondes aux appels serveur pour la fabrication d’objets. Le délai par défaut est 500.")
+mkstr(SI.DEFAULT_MAX,		"MultiCraft: La valeur par défaut du slider est max.")
+mkstr(SI.DEFAULT_MIN,		"MultiCraft: La valeur par défaut du slider est 1.")
+mkstr(SI.TRAITS_ON,			"MultiCraft: Utilisation des caractéristiques activée.")
+mkstr(SI.TRAITS_OFF,		"MultiCraft: Utilisation des caractéristiques désactivée.")
+mkstr(SI.CALL_DELAY,		"MultiCraft: Fréquence de fabrication limitée à un appel toutes les %d millisecondes.")
